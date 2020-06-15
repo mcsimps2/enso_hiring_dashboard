902caeeb59e708ef4f0483f1b2deeca02fbfa12c
Matt Simpson <mcsimps2@gmail.com>
2020-06-15 18:59:41 -0400

    Add placeholder Paginator component in molecules
    This is merely to help developers know where to place the pagination component they develop in case they are not familiar with the atomic design project structure.

---
+++ src/components/molecules/Paginator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// You may write your pagination component here
+// Feel free to setup the component according to your own coding style
+// (e.g. subclasses Component vs. using React hooks, declaring prop types, etc...)
+const Paginator = (props) => {
+    return null;
+};
+
+export default Paginator;
